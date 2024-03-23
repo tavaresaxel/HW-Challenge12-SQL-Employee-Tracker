c921dc7f4ca1fda0fef1c9215f5dfee1dfc82427
axel tavares <tavaresaxel95@gmail.com>
2024-03-22 21:32:56 -0400

    intalled necessary npms and started inplementing inquirer prompts

--- index.js
+++ index.js
@@ -2,4 +2,27 @@ const inquirer = require("inquirer")
 const {printTable} = require("console-table-printer")
 const mysql2 = require("mysql2")
 
-const db = mysql2.createConnections()
+const db = mysql2.createConnections({
+    host:"localhost",
+    user:"root",
+    password:"password",
+    database:"employee_db",
+    port:3306
+})
+
+db.connect( () => {
+
+})
+
+
+function menu(){
+//view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+inquirer.prompt([
+    {
+        
+    }
+])
+
+
+}
